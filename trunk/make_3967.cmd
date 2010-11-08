6b068413b5ec98f65ba716fae2fb371e62f04fc9
Malandrinus2009 <Malandrinus2009@627091e6-4dbd-6d29-6aae-745477cf1ff5>
2010-11-08 06:13:55 +0000

    reorganization of directories and files

---
+++ trunk/make_3967.cmd
@@ -0,0 +1,17 @@
+@echo off
+cd 3967_cop_1602
+if exist mydll.obj del mydll.obj
+if exist mydll.dll del mydll.dll
+if exist mydll.exp del mydll.exp
+if exist mydll.lib del mydll.lib
+if exist xrGame.dll del xrGame.dll
+
+c:\masm32\bin\ml /c /coff /Fo mydll.obj mydll.asm
+c:\masm32\bin\Link /SUBSYSTEM:WINDOWS /DLL /OUT:mydll.dll /DEF:mydll.def mydll.obj 
+..\tools\bspatch.exe xrGame_orig.dll xrGame.dll xrGame.diff
+..\tools\patcher.exe xrGame.dll mydll.dll corrections_list.txt
+
+del /Q mydll.obj
+del /Q mydll.dll
+del /Q mydll.exp
+del /Q mydll.lib
--- trunk/patch.cmd
+++
@@ -1,3 +0,0 @@
-copy /Y xrGame_[to_patch].dll xrGame.dll
-patcher.exe xrGame.dll mydll.dll corrections_list.txt
-pause
