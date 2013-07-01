8c4772fb047ea4f5b09de39e53dbc3218e9afcc9
Kdementev@gmail.com <Kdementev@gmail.com@627091e6-4dbd-6d29-6aae-745477cf1ff5>
2013-07-01 19:31:50 +0000

    [render_soc_r1/r2] + add command for bloodmarks lifetime tuning (r__bloodmarks_lifetime)

--- branches/ogse_0693/exe/console_comm.asm
+++ branches/ogse_0693/exe/console_comm.asm
@@ -2,7 +2,6 @@ con_comm:
 
 	mov     dword ptr [eax], offset xCCC_Integer
 	
-REGISTER_CC_FLOAT sunshafts_exposure, "r2_script_sun_shafts", 0.0, 1.0	
 REGISTER_CC_FLOAT wm_lifetime, "r__bloodmarks_lifetime", 0.001, 1.0	
 
 	; делаем вырезанное	
@@ -12,8 +11,3 @@ REGISTER_CC_FLOAT wm_lifetime, "r__bloodmarks_lifetime", 0.001, 1.0
 	pop		ebx
 	pop		ecx
 	retn
-
-rain_drops_switch dd 0.0
-sunshafts_exposure dd 1.0
-color_grading_density dd 1.0
-color_grading_color dd 3 dup(0.0)
