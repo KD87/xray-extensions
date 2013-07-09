674b44c0effecf082337b43163495d1338a431ac
Kdementev@gmail.com <Kdementev@gmail.com@627091e6-4dbd-6d29-6aae-745477cf1ff5>
2013-07-09 17:33:56 +0000

    [soc_render_r2] ~ исправил баг в фиксе мигания травы / fixed bug in detail blink fix

--- trunk/render/3312_shoc_10006/R2/detail_blink_fix.asm
+++ trunk/render/3312_shoc_10006/R2/detail_blink_fix.asm
@@ -7,6 +7,6 @@ detail_blink_fix:
 	add		esi, 8
 	; copy viewbase into current frustum
 	mov		ecx, 3Dh
-	lea     edi, [esp+14Ch-0F8h]
+	lea     edi, [esp+148h-0F8h]
 	rep movsd
 	jmp back_from_detail_blink_fix
