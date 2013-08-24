274d82922193ac7a47905874ab602fe3e33dadbe
Malandrinus2009 <Malandrinus2009@gmail.com>
2013-08-24 22:39:12 +0000

    * временно отключена консольная команда r__bloodmarks_lifetime.

--- exe/console_comm.asm
+++ exe/console_comm.asm
@@ -2,7 +2,7 @@ con_comm:
 
 	mov     dword ptr [eax], offset xCCC_Integer
 	
-REGISTER_CC_FLOAT wm_lifetime, "r__bloodmarks_lifetime", 0.001, 1.0	
+;REGISTER_CC_FLOAT wm_lifetime, "r__bloodmarks_lifetime", 0.001, 1.0	
 
 	; делаем вырезанное	
 	pop		edi
