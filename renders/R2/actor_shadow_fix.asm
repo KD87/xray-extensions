05891bba8d65e791c5ded68c18cd6eb84a2f9009
kdementev <Kdementev@gmail.com>
2013-11-17 16:32:51 +0000

    ТЧ: [+] добавлена тень от оружия ГГ в оба рендера [~] проверка на тип рендера сделана побыстрее

--- renders/R2/actor_shadow_fix.asm
+++ renders/R2/actor_shadow_fix.asm
@@ -1,5 +1,11 @@
 actor_shadow_fix:
 	mov		eax, ds:g_pGameLevel
+	test	eax, eax
+	jz		exit
+	
+; 4253E3 - CHUDManager::Render_First()  set_invisible (false/true)
+; 1C8948 - CActor::renderable_Render() убрали hudview
+	
 	mov		eax, [eax]
 	push	eax
 	mov     eax, [eax+148h]
@@ -8,11 +14,9 @@ actor_shadow_fix:
 	mov     eax, [edx+0Ch]
 	call    eax
 	pop		eax
-	mov     eax, [eax+148h]
-	mov     edx, [eax]
-	mov     ecx, eax
-	mov     eax, [edx+10h]
-	call    eax
+	
+exit:
+	; old
 	add     [edi+320h], ebx
 
 	jmp back_to_actor_shadow_fix
