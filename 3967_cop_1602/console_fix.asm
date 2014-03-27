32719d0d62d2884dac7f4e55658453697ec49a4e
RayTwitty <RayTwitty@gmail.com>
2014-03-27 20:38:41 +0000

    [CoP_1602_xrGame] 1. В пространство level добавлены следующие методы: int level.get_actor_body_state(void) - возвращает текущее состояние тела ГГ (идет, бежит и т.п.) void level.set_fov(float) - установка fov камеры ГГ float level.get_fov(void) - получение текущего fov камеры ГГ 2. Восстановлена консольная команда hud_fov [0.0, 1.0] - дистанция от камеры до худа оружия 3. Поправлены пределы консольных команд cam_fov [55.0, 90.0] (переименовано из fov) и ph_timefactor [0.0, 10.0]

--- 3967_cop_1602/console_fix.asm
+++ 3967_cop_1602/console_fix.asm
@@ -1,27 +1,26 @@
 ; Добавляет консольные команды:
-;  	fov 			- угол обзора игрока
+;  	cam_fov 		- угол обзора игрока
+;  	hud_fov 		- дистанция от камеры до худа оружия
 ;	ph_timefactor	- скорость течения времени при просчете физики
 
 ; Флаг. Показывает, что консольные команды инициализированы
 CommandCreated	dd	0
 
 ; Параметры консольной команды fov
-fov_min		REAL4	5.0					; минимальное значение
-fov_max		REAL4	180.0				; максимальное значение
-fov_cmd		db 		"fov",0				; название
-
+fov_min		REAL4	55.0				; минимальное значение
+fov_max		REAL4	90.0				; максимальное значение
+fov_cmd		db 		"cam_fov",0			; название
 g_fov_ccc	db 		36 dup(?)			;CCC_Float g_fov_ccc
 
 ; Параметры консольной команды hud_fov
-; Не работает из-за того, что не смог импортировать в xrgame.dll g_hud_fov
-;hud_fov_min	REAL4 	0.1
-;hud_fov_max	REAL4	1.0
-;hud_fov_cmd	db 		"hud_fov",0
-;g_hud_fov_ccc	db 		36 dup(?)
+hud_fov_min	REAL4	0.0					; минимальное значение
+hud_fov_max	REAL4	1.0					; максимальное значение
+hud_fov_cmd	db 		"hud_fov",0			; название
+g_hud_fov_ccc	db 	36 dup(?)			;CCC_Float g_hud_fov_ccc
 
 ; Параметры консольной команды ph_timefactor
-ph_tf_min	REAL4	0.0001				; минимальное значение
-ph_tf_max	REAL4	1000.0				; максимальное значение
+ph_tf_min	REAL4	0.0					; минимальное значение
+ph_tf_max	REAL4	10.0				; максимальное значение
 ph_tf_cmd	db 		"ph_timefactor",0	; название
 g_ph_tf_ccc	db 		36 dup(?)			;CCC_Float g_ph_tf_ccc
 
@@ -31,10 +30,11 @@ fov_dtor proc
 	jmp     ds:_1CCC_Float_ ; CCC_Float::~CCC_Float(void)
 fov_dtor endp
 
-;hud_fov_dtor proc
-;	mov     ecx, offset g_hud_fov_ccc
-;	jmp     ds:_1CCC_Float_ ; CCC_Float::~CCC_Float(void)
-;hud_fov_dtor endp
+; По факту деструктор статической переменной g_hud_fov_ccc
+hud_fov_dtor proc
+	mov     ecx, offset g_hud_fov_ccc
+	jmp     ds:_1CCC_Float_ ; CCC_Float::~CCC_Float(void)
+hud_fov_dtor endp
 
 ; По факту деструктор статической переменной g_ph_tf_ccc
 ph_tf_dtor proc
@@ -76,29 +76,28 @@ m1:
 	push    offset g_fov_ccc
 	call    esi 					; CConsole::AddCommand(IConsole_Command *) ; CConsole::AddCommand(IConsole_Command *)
 
-;	mov     eax, 2
-;	test    CommandCreated, eax
-;	jnz     m2
-;	fld     ds:hud_fov_max
-;	mov     ecx, ds:_psHUD_FOV_ ; float psHUD_FOV
-;	or      CommandCreated, eax
-;	sub     esp, 8
-;	fstp    [esp+10h+var_C]
-;	fld     ds:hud_fov_min
-;	fstp    [esp+10h+var_10]
-;	push    ecx
-;	push    offset hud_fov_cmd
-;	mov     ecx, offset g_hud_fov_ccc
-;	call    ds:_CCC_Float_ ; CCC_Float::CCC_Float(char const *,float *,float,float)
-;	push    offset hud_fov_dtor ; void (__cdecl *)()
-;	call    _atexit
-;	add     esp, 4
-;
-;m2: 
-;	mov     edx, ds:_Console_ ; CConsole * Console
-;	mov     ecx, [edx]
-;	push    offset g_hud_fov_ccc
-;	call    esi ; CConsole::AddCommand(IConsole_Command *) ; CConsole::AddCommand(IConsole_Command *)	
+	mov     eax, 2
+	test    CommandCreated, eax
+	jnz     m2
+	fld     ds:hud_fov_max
+	or      CommandCreated, eax
+	sub     esp, 8
+	fstp    [esp+10h+var_C]
+	fld     ds:hud_fov_min
+	fstp    [esp+10h+var_10]
+	push    00490624h ; float psHUD_FOV
+	push    offset hud_fov_cmd
+	mov     ecx, offset g_hud_fov_ccc
+	call    ds:_CCC_Float_ ; CCC_Float::CCC_Float(char const *,float *,float,float)
+	push    offset hud_fov_dtor ; void (__cdecl *)()
+	call    _atexit
+	add     esp, 4
+
+m2: 
+	mov     edx, ds:_Console_ ; CConsole * Console
+	mov     ecx, [edx]
+	push    offset g_hud_fov_ccc
+	call    esi ; CConsole::AddCommand(IConsole_Command *) ; CConsole::AddCommand(IConsole_Command *)
 
 ;проверяем, что команда еще не создана
 	mov     eax, 4
--- 3967_cop_1602/level_ns_fix.asm
+++ 3967_cop_1602/level_ns_fix.asm
@@ -18,10 +18,37 @@ level_ns_extension_1:
 	push    eax
 	call    object_by_id_register ; регистрируем функцию с прототипом как у   object_by_id
 ;--------------------------------------
+	pop     ecx
+	pop     ecx
+	mov     eax, esp
+	push    offset GetActorBodyState
+	push    offset aGet_actor_body_state ; "get_actor_body_state"
+	push    eax
+	call    get_time_days_register ; регистрируем функцию с прототипом как у   get_time_days
+;--------------------------------------
+	pop     ecx
+	pop     ecx
+	mov     eax, esp
+	push    offset SetFov
+	push    offset aSet_fov ; "set_fov"
+	push    eax
+	call    set_snd_volume_register ; регистрируем функцию с прототипом как у   set_snd_volume
+;--------------------------------------
+	pop     ecx
+	pop     ecx
+	mov     eax, esp
+	push    offset GetFov
+	push    offset aGet_fov ; "get_fov"
+	push    eax
+	call    get_snd_volume_register ; регистрируем функцию с прототипом как у   get_snd_volume
+;--------------------------------------
 	jmp back_to_level_ns_ext_1
 	
 aGet_target_dist db "get_target_dist", 0
 aGet_target_obj  db "get_target_obj", 0
+aGet_actor_body_state db "get_actor_body_state", 0
+aSet_fov db "set_fov", 0
+aGet_fov db "get_fov", 0
 
 level_ns_extension_2: ; здесь надо добавлять столько раз   "mov ecx, eax" + "call esi", сколько добавляли функций
 ; делаем то, что вырезали
@@ -34,6 +61,15 @@ level_ns_extension_2: ; здесь надо добавлять столько раз   "mov ecx, eax" + "cal
 	mov     ecx, eax
 	call    esi 
 	; для get_target_obj
+	mov     ecx, eax
+	call    esi 
+	; для get_actor_body_state
+	mov     ecx, eax
+	call    esi 
+	; для set_fov
+	mov     ecx, eax
+	call    esi 
+	; для get_fov
 	mov     ecx, eax
 	call    esi 
 ; идём обратно
@@ -61,3 +97,24 @@ GetTargetObject:
 return_value: ; иначе - конвертируем в game_object
 	mov     ecx, eax ; this = <найденный CGameObject>
 	jmp     CGameObject__lua_game_object
+
+GetActorBodyState proc
+	mov     eax, [g_Actor]
+	test    eax, eax
+	jz      short exit_fail
+	mov     eax, [eax+1428] ; mstate_real (0x594 = 1428)
+exit_fail:
+	retn
+GetActorBodyState endp
+
+SetFov proc
+arg_0           = dword ptr  4
+	mov     eax, [esp+arg_0]
+	mov     [g_fov], eax
+	retn
+SetFov endp
+
+GetFov proc
+	fld     dword ptr [g_fov]
+	retn
+GetFov endp
