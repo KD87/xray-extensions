4186936c5deebfb8ca676c2d04b63dae4b4086a7
RayTwitty <RayTwitty@gmail.com>
2014-08-01 21:13:06 +0000

    [SoC_10006_xrGame] 1. Восстановлены консольные команды: cam_fov [55.0, 90.0] - FOV камеры ГГ hud_fov [0.0, 1.0] - дистанция от камеры до худа оружия ph_timefactor [0.0, 10.0] - время просчета физики ph_gravity [0.0, 100.0] - гравитация (изменение работает только при перезагрузке игры) g_god [on/off] - режим бога g_unlimitedammo [on/off] - режим бесконечных боеприпасов 2. Отключены консольные команды мода OGSE 3. Дефолтное значение чувствительности мыши в меню установлено на аналогичное патчу 1.0007 4. Значения для правки инерции худа приведены в дефолтное состояние

--- 3312_shoc_10006/console_commands_reg_fix.asm
+++ 3312_shoc_10006/console_commands_reg_fix.asm
@@ -15,11 +15,26 @@ REGISTER_CC_FLAG g_ammo_on_belt, 1h, "g_ammunition_on_belt"
 
 REGISTER_CC_FLAG g_mouse_wheel_sc, 1h, "mouse_wheel_slot_changing"
 
-REGISTER_CC_INT2   g_ogse_nv_bind,           "ogse_nv_bind",           0, 07FFFFFFFh
-REGISTER_CC_INT2   g_ogse_antirad_bind,      "ogse_antirad_bind",      0, 07FFFFFFFh
-REGISTER_CC_INT2   g_ogse_energy_drink_bind, "ogse_energy_drink_bind", 0, 07FFFFFFFh
-REGISTER_CC_INT2   g_ogse_yod_bind,          "ogse_yod_bind",          0, 07FFFFFFFh
-REGISTER_CC_INT2   g_ogse_bipsizon_bind,     "ogse_bipsizon_bind",     0, 07FFFFFFFh
+;REGISTER_CC_INT2   g_ogse_nv_bind,           "ogse_nv_bind",           0, 07FFFFFFFh
+;REGISTER_CC_INT2   g_ogse_antirad_bind,      "ogse_antirad_bind",      0, 07FFFFFFFh
+;REGISTER_CC_INT2   g_ogse_energy_drink_bind, "ogse_energy_drink_bind", 0, 07FFFFFFFh
+;REGISTER_CC_INT2   g_ogse_yod_bind,          "ogse_yod_bind",          0, 07FFFFFFFh
+;REGISTER_CC_INT2   g_ogse_bipsizon_bind,     "ogse_bipsizon_bind",     0, 07FFFFFFFh
+
+; =========================================================================================
+; ========================= added by Ray Twitty (aka Shadows) =============================
+; =========================================================================================
+; ====================================== START ============================================
+; =========================================================================================
+REGISTER_CC_FLOAT2 g_fov, "cam_fov", 55.0, 90.0
+REGISTER_CC_FLOAT2 00506BA4h, "hud_fov", 0.0, 1.0
+REGISTER_CC_FLOAT2 phTimefactor, "ph_timefactor", 0.0, 10.0
+REGISTER_CC_FLOAT2 phGravity, "ph_gravity", 0.0, 100.0
+REGISTER_CC_FLAG psActorFlags, 8, "g_unlimitedammo"
+REGISTER_CC_FLAG psActorFlags, 1, "g_god"
+; =========================================================================================
+; ======================================= END =============================================
+; =========================================================================================
 
 	; делаем то, что вырезали, включая возврат из функции
 	pop     edi
@@ -28,16 +43,14 @@ REGISTER_CC_INT2   g_ogse_bipsizon_bind,     "ogse_bipsizon_bind",     0, 07FFFF
 	retn
 add_console_commands_fix endp
 
-g_ui_mouse_sens dd 1.5
-
-g_ftest dd 0.123
-g_itest dd 123
-g_test_flags dd 0
+g_ui_mouse_sens dd 3.3
 g_ammo_on_belt dd 0
 g_mouse_wheel_sc dd 1
 
-g_ogse_nv_bind           dd 100000
-g_ogse_antirad_bind      dd 100000
-g_ogse_energy_drink_bind dd 100000
-g_ogse_yod_bind          dd 100000
-g_ogse_bipsizon_bind     dd 100000
+;g_ogse_nv_bind           dd 100000
+;g_ogse_antirad_bind      dd 100000
+;g_ogse_energy_drink_bind dd 100000
+;g_ogse_yod_bind          dd 100000
+;g_ogse_bipsizon_bind     dd 100000
+
+phGravity dd 19.62
