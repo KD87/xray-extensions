f61cd627f646887abd27143aeb40ec58c5257b47
RayTwitty@gmail.com <RayTwitty@gmail.com@627091e6-4dbd-6d29-6aae-745477cf1ff5>
2014-08-07 17:22:47 +0000

    [SoC_10006_XR_3DA] 1. Восстановлена потерявшаяся правка по увеличению времени жизни блудмарков. Вместо консольной команды время теперь фиксировано - 16 реальных минут. Более чем достаточно. 2. Переделана правка убирающая сообщения из лога: "cl setDestroy [%d][%d]". 3. Почищены остатки правок, которые используются только в моде OGSE. 4. Отключена по умолчанию правка "полноразмерные экраны загрузок". 5. Консольная команда "r2_script_sun_shafts" переименована в "r2_sun_shafts_intensity".

--- trunk/exe/3312_shoc_10006/console_comm.asm
+++ trunk/exe/3312_shoc_10006/console_comm.asm
@@ -2,8 +2,7 @@ con_comm:
 
 	mov     dword ptr [eax], offset xCCC_Integer
 	
-REGISTER_CC_FLOAT sunshafts_exposure, "r2_script_sun_shafts", 0.0, 1.0	
-REGISTER_CC_FLOAT wm_lifetime, "r__bloodmarks_lifetime", 0.001, 1.0	
+REGISTER_CC_FLOAT sunshafts_exposure, "r2_sun_shafts_intensity", 0.0, 1.0
 
 	; делаем вырезанное	
 	pop		edi
@@ -13,7 +12,4 @@ REGISTER_CC_FLOAT wm_lifetime, "r__bloodmarks_lifetime", 0.001, 1.0
 	pop		ecx
 	retn
 
-rain_drops_switch dd 0.0
 sunshafts_exposure dd 1.0
-color_grading_density dd 1.0
-color_grading_color dd 3 dup(0.0)
