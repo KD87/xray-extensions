45d8df6ac9094ecff84a9fe3267eaf01e3abf9f7
kdementev@gmail.com <kdementev@gmail.com@627091e6-4dbd-6d29-6aae-745477cf1ff5>
2014-11-08 13:07:45 +0000

    ! переделал правку на неотключение света аномалий при их отключении

--- trunk/3312_shoc_10006/customzone_fix.asm
+++ trunk/3312_shoc_10006/customzone_fix.asm
@@ -1,4 +1,32 @@
-customzonefix:
-	xor		eax, eax
-	mov		[esi+2FCh], eax
-	jmp		back_from_customzonefix
+customzonefix proc
+;	xor		eax, eax
+;	mov		[esi+2FCh], eax
+;	jmp		back_from_customzonefix
+
+	; проверка, есть ли свет
+	mov     esi, [ecx+2E0h]
+	mov     eax, esi
+	neg     eax
+	sbb     eax, eax
+	test    ds:resptr_base_IRender_Light____get, eax ; resptr_base<IRender_Light>::_get(void)
+	jz      short exit
+	; проверка, включен ли свет
+	mov     edx, [esi]
+	mov     eax, [edx+8]
+	call    eax
+	test    al, al
+	jz      short exit
+	; вылючаем свет, если включен
+	mov     esi, [ecx+2E0h]
+	xorps   xmm1, xmm1
+	movss   dword ptr [ecx+2FCh], xmm1		;m_fLightTimeLeft = 0.f;
+	mov     eax, [esi]
+	mov     edx, [eax+4]
+	push    0
+	call    edx								;m_pLight->set_active(false);
+
+exit:
+	pop		edi
+	pop		esi
+	retn 4
+customzonefix endp
