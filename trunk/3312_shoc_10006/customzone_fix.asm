344f2aebc8a19a8e36dec281ebd552ffa7bd6d0b
kdementev@gmail.com <kdementev@gmail.com@627091e6-4dbd-6d29-6aae-745477cf1ff5>
2014-11-22 17:26:50 +0000

    ! теперь не только работает, но и не вылетает при загрузке :)

--- trunk/3312_shoc_10006/customzone_fix.asm
+++ trunk/3312_shoc_10006/customzone_fix.asm
@@ -1,32 +1,31 @@
 customzonefix proc
-	xorps   xmm1, xmm1
-	movss   dword ptr [ecx+2FCh], xmm1		;m_fLightTimeLeft = 0.f;
-	jmp		back_from_customzonefix
-
-	; проверка, есть ли свет
-;	mov     esi, [ecx+2E0h]
-;	mov     eax, esi
-;	neg     eax
-;	sbb     eax, eax
-;	test    ds:resptr_base_IRender_Light____get, eax ; resptr_base<IRender_Light>::_get(void)
-;	jz      short exit
-	; проверка, включен ли свет
-;	mov     edx, [esi]
-;	mov     eax, [edx+8]
-;	call    eax
-;	test    al, al
-;	jz      short exit
-	; вылючаем свет, если включен
-;	mov     esi, [ecx+2E0h]
 ;	xorps   xmm1, xmm1
 ;	movss   dword ptr [ecx+2FCh], xmm1		;m_fLightTimeLeft = 0.f;
-;	mov     eax, [esi]
-;	mov     edx, [eax+4]
-;	push    0
-;	call    edx								;m_pLight->set_active(false);
+;	mov		esi, ecx
+;	jmp		back_from_customzonefix
 
-;exit:
-;	pop		edi
-;	pop		esi
-;	retn 4
+	; выключаем свет выброса
+	xorps   xmm1, xmm1
+	movss   dword ptr [ecx+2FCh], xmm1		;m_fLightTimeLeft = 0.f;
+	; проверка, есть ли постоянный свет
+	mov     esi, [ecx+2BCh]
+	test    esi, esi
+	jz      short exit
+	; проверка, включен ли постоянный свет
+	mov     edx, [esi]
+	mov     eax, [edx+8]
+	call    eax
+	test    al, al
+	jz      short exit
+	; вылючаем постоянный свет, если включен
+	mov     eax, [edx+4]
+	push    0
+	call    eax								;m_pLight->set_active(false);
+
+exit:
+;	mov		esi, ecx
+;	jmp		back_from_customzonefix
+	pop		edi
+	pop		esi
+	retn 4
 customzonefix endp
