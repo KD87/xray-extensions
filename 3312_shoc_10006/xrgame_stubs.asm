a224b12833a97ac53d5ad081eb8b044892ee673c
kdementev <Kdementev@gmail.com>
2014-11-22 12:36:09 +0000

    ! доработал фикс выключения света при дизейбле аномалии, теперь точно работает

--- 3312_shoc_10006/xrgame_stubs.asm
+++ 3312_shoc_10006/xrgame_stubs.asm
@@ -4276,6 +4276,6 @@ back_from_game_object_destructor_fix:
 org 10257BAFh - shift
 	jmp	customzonefix
 
-;org 10257BB4h - shift
-;back_from_customzonefix:
+org 10257BB4h - shift
+back_from_customzonefix:
 	
