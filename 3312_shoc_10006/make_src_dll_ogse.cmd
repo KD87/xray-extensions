b96bf8fb4219cf24884ab2f28885f75052b17698
malandrinus <malandrinus2009@gmail.com>
2015-11-19 09:12:28 -0500

    + батник для ассемблирования под OGSE ~ раскомментированы и внесены в конфигурацию OGSE патчи для расцветки предметов в инвентаре

---
+++ 3312_shoc_10006/make_src_dll_ogse.cmd
@@ -0,0 +1,16 @@
+@echo off
+if exist mydll.obj del mydll.obj
+if exist mydll.dll del mydll.dll
+if exist mydll.exp del mydll.exp
+if exist mydll.lib del mydll.lib
+
+..\tools\ml /c /coff /DOGSE_BUILD /Fo mydll.obj mydll.asm
+C:\masm32\bin\link  /SUBSYSTEM:WINDOWS /DLL /OUT:mydll.dll /DEF:mydll.def mydll.obj 
+rem /SAFESEH:NO /MAP:map.txt
+
+
+if exist mydll.obj del mydll.obj
+if exist mydll.exp del mydll.exp
+if exist mydll.lib del mydll.lib
+
+pause
