d41c35c0386c9b086659f4f80e6a086c653d289c
malandrinus <malandrinus2009@gmail.com>
2015-11-20 22:42:04 -0500

    ~ все файлы, связанные со сборкой и конфигурацией, перенесены в папку "build"

--- 3312_shoc_10006/build/make_src_dll_ogse.cmd
+++ 3312_shoc_10006/build/make_src_dll_ogse.cmd
@@ -4,8 +4,8 @@ if exist mydll.dll del mydll.dll
 if exist mydll.exp del mydll.exp
 if exist mydll.lib del mydll.lib
 
-..\tools\ml /c /coff /DOGSE_BUILD /Fo mydll.obj mydll.asm
-C:\masm32\bin\link  /SUBSYSTEM:WINDOWS /DLL /OUT:mydll.dll /DEF:mydll.def mydll.obj 
+..\..\tools\ml /c /coff /DOGSE_BUILD /Fo mydll.obj ..\mydll.asm
+C:\masm32\bin\link  /SUBSYSTEM:WINDOWS /DLL /OUT:mydll.dll /DEF:..\mydll.def mydll.obj
 rem /SAFESEH:NO /MAP:map.txt
 
 
--- 3312_shoc_10006/clean.cmd
+++
@@ -1,6 +0,0 @@
-if exist mydll.obj del mydll.obj
-if exist mydll.dll del mydll.dll
-if exist mydll.exp del mydll.exp
-if exist mydll.lib del mydll.lib
-if exist xrGame.dll del xrGame.dll
-if exist corrections_list_tmp.txt del corrections_list_tmp.txt
